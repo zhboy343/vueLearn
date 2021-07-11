3ba8b8eb6e9fd33b492b9d892ad91a71091e4414
zhouhang <1024223787@qq.com>
2021-07-11 22:32:41 +0800

    底部菜单样式

--- tabbar/src/App.vue
+++ tabbar/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="tar-bar">
-      <div>首页</div>
-      <div>分类</div>
-      <div>购物车</div>
-      <div>我的</div>
+      <div class="tab-bar-item">首页</div>
+      <div class="tab-bar-item">分类</div>
+      <div class="tab-bar-item">购物车</div>
+      <div class="tab-bar-item">我的</div>
     </div>
   </div>
 </template>
@@ -18,4 +18,22 @@ export default {
 
 <style>
 @import "./assets/css/base.css";
+#tar-bar {
+  display: flex;
+
+  background-color: #f6f6f6;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  /** x偏移量 y偏移量  虚化 范围 颜色*/
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.1);
+}
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
 </style>
