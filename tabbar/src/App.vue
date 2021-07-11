76ba4b9c4a6145047ec0b0f69764af7d541bde6e
zhouhang <1024223787@qq.com>
2021-07-11 22:06:30 +0800

    删除默认属性

--- tabbar/src/App.vue
+++ tabbar/src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="tar-bar">
+      <div>首页</div>
+      <div>分类</div>
+      <div>购物车</div>
+      <div>我的</div>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
