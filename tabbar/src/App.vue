b0d2234498571b739854763a6d0913f036473f78
zhouhang <1024223787@qq.com>
2021-07-11 22:43:28 +0800

    底部菜单抽成components

--- tabbar/src/App.vue
+++ tabbar/src/App.vue
@@ -1,39 +1,20 @@
 <template>
   <div id="app">
-    <div id="tar-bar">
-      <div class="tab-bar-item">首页</div>
-      <div class="tab-bar-item">分类</div>
-      <div class="tab-bar-item">购物车</div>
-      <div class="tab-bar-item">我的</div>
-    </div>
+    <TabBar></TabBar>
   </div>
 </template>
 
 <script>
+import TabBar from "./components/tabbar/TabBar.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    TabBar,
+  },
 };
 </script>
 
 <style>
 @import "./assets/css/base.css";
-#tar-bar {
-  display: flex;
-
-  background-color: #f6f6f6;
-
-  position: fixed;
-  left: 0;
-  right: 0;
-  bottom: 0;
-
-  /** x偏移量 y偏移量  虚化 范围 颜色*/
-  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.1);
-}
-.tab-bar-item {
-  flex: 1;
-  text-align: center;
-  height: 49px;
-}
 </style>
---
+++ tabbar/src/components/tabbar/TabBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="tar-bar">
+    <div class="tab-bar-item">首页</div>
+    <div class="tab-bar-item">分类</div>
+    <div class="tab-bar-item">购物车</div>
+    <div class="tab-bar-item">我的</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TarBar",
+  components: {},
+};
+</script>
+
+<style>
+#tar-bar {
+  display: flex;
+
+  background-color: #f6f6f6;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  /** x偏移量 y偏移量  虚化 范围 颜色*/
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.1);
+}
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+</style>
