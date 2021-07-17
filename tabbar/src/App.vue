476189eb18b6f5528a46c2f6e83edb9e43689a70
zhouhang <1024223787@qq.com>
2021-07-17 20:49:07 +0800

    MainTabBar抽取

--- tabbar/src/App.vue
+++ tabbar/src/App.vue
@@ -2,52 +2,17 @@
   <div id="app">
     <!-- 占位-用于显示组件页面 -->
     <router-view></router-view>
-    <TabBar>
-      <TabBarItem path="/home" activeColor="#204288">
-        <img slot="item-icon" src="./assets/img/tarbar/home.svg" />
-        <img
-          slot="item-icon-active"
-          src="./assets/img/tarbar/home_active.svg"
-        />
-        <div slot="item-text">首页</div>
-      </TabBarItem>
-      <TabBarItem path="/category" activeColor="#204288">
-        <img slot="item-icon" src="./assets/img/tarbar/category.svg" />
-        <img
-          slot="item-icon-active"
-          src="./assets/img/tarbar/category_active.svg"
-        />
-        <div slot="item-text">分类</div>
-      </TabBarItem>
-      <TabBarItem path="/shopcart" activeColor="#ff5777">
-        <img slot="item-icon" src="./assets/img/tarbar/shopcart.svg" />
-        <img
-          slot="item-icon-active"
-          src="./assets/img/tarbar/shopcart_active.svg"
-        />
-        <div slot="item-text">购物车</div>
-      </TabBarItem>
-      <TabBarItem path="/profile">
-        <img slot="item-icon" src="./assets/img/tarbar/profile.svg" />
-        <img
-          slot="item-icon-active"
-          src="./assets/img/tarbar/profile_active.svg"
-        />
-        <div slot="item-text">我的</div>
-      </TabBarItem>
-    </TabBar>
+    <MainTabBar></MainTabBar>
   </div>
 </template>
 
 <script>
-import TabBar from "./components/tabbar/TabBar.vue";
-import TabBarItem from "./components/tabbar/TabBarItem.vue";
+import MainTabBar from "./components/MainTabBar.vue";
 
 export default {
   name: "App",
   components: {
-    TabBar,
-    TabBarItem,
+    MainTabBar,
   },
 };
 </script>
---
+++ tabbar/src/components/MainTabBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <TabBar>
+    <TabBarItem path="/home" activeColor="#204288">
+      <img slot="item-icon" src="../assets/img/tarbar/home.svg" />
+      <img slot="item-icon-active" src="../assets/img/tarbar/home_active.svg" />
+      <div slot="item-text">首页</div>
+    </TabBarItem>
+    <TabBarItem path="/category" activeColor="#204288">
+      <img slot="item-icon" src="../assets/img/tarbar/category.svg" />
+      <img
+        slot="item-icon-active"
+        src="../assets/img/tarbar/category_active.svg"
+      />
+      <div slot="item-text">分类</div>
+    </TabBarItem>
+    <TabBarItem path="/shopcart" activeColor="#ff5777">
+      <img slot="item-icon" src="../assets/img/tarbar/shopcart.svg" />
+      <img
+        slot="item-icon-active"
+        src="../assets/img/tarbar/shopcart_active.svg"
+      />
+      <div slot="item-text">购物车</div>
+    </TabBarItem>
+    <TabBarItem path="/profile">
+      <img slot="item-icon" src="../assets/img/tarbar/profile.svg" />
+      <img
+        slot="item-icon-active"
+        src="../assets/img/tarbar/profile_active.svg"
+      />
+      <div slot="item-text">我的</div>
+    </TabBarItem>
+  </TabBar>
+</template>
+
+<script>
+import TabBar from "./tabbar/TabBar.vue";
+import TabBarItem from "./tabbar/TabBarItem.vue";
+
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem,
+  },
+};
+</script>
+
+<style>
+</style>
