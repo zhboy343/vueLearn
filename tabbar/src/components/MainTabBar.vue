49eb1b78b0031dbc71401f4014541afa8f30fbab
zhouhang <1024223787@qq.com>
2021-07-17 22:31:12 +0800

    使用添加的别名---非import情况下时,如src。在别名前加上~

--- tabbar/src/components/MainTabBar.vue
+++ tabbar/src/components/MainTabBar.vue
@@ -1,31 +1,31 @@
 <template>
   <TabBar>
     <TabBarItem path="/home" activeColor="#204288">
-      <img slot="item-icon" src="@/assets/img/tarbar/home.svg" />
-      <img slot="item-icon-active" src="@/assets/img/tarbar/home_active.svg" />
+      <img slot="item-icon" src="~assets/img/tarbar/home.svg" />
+      <img slot="item-icon-active" src="~assets/img/tarbar/home_active.svg" />
       <div slot="item-text">首页</div>
     </TabBarItem>
     <TabBarItem path="/category" activeColor="#204288">
-      <img slot="item-icon" src="@/assets/img/tarbar/category.svg" />
+      <img slot="item-icon" src="~assets/img/tarbar/category.svg" />
       <img
         slot="item-icon-active"
-        src="@/assets/img/tarbar/category_active.svg"
+        src="~assets/img/tarbar/category_active.svg"
       />
       <div slot="item-text">分类</div>
     </TabBarItem>
     <TabBarItem path="/shopcart" activeColor="#ff5777">
-      <img slot="item-icon" src="@/assets/img/tarbar/shopcart.svg" />
+      <img slot="item-icon" src="~assets/img/tarbar/shopcart.svg" />
       <img
         slot="item-icon-active"
-        src="@/assets/img/tarbar/shopcart_active.svg"
+        src="~assets/img/tarbar/shopcart_active.svg"
       />
       <div slot="item-text">购物车</div>
     </TabBarItem>
     <TabBarItem path="/profile">
-      <img slot="item-icon" src="@/assets/img/tarbar/profile.svg" />
+      <img slot="item-icon" src="~assets/img/tarbar/profile.svg" />
       <img
         slot="item-icon-active"
-        src="@/assets/img/tarbar/profile_active.svg"
+        src="~assets/img/tarbar/profile_active.svg"
       />
       <div slot="item-text">我的</div>
     </TabBarItem>
