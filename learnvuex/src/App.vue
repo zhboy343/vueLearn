94328dd7567f8d0c0dea4aed926f313e58d96308
zhouhang <1024223787@qq.com>
2021-08-04 22:26:22 +0800

    vuex学习--基础项目创建2

--- learnvuex/src/App.vue
+++ learnvuex/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
     <h2>{{ message }}</h2>
+    <App2></App2>
   </div>
 </template>
 
 <script>
+import App2 from "./components/App2.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    App2,
+  },
   data() {
     return {
       message: "ssssss",
---
+++ learnvuex/src/components/App2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app2">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App2",
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
