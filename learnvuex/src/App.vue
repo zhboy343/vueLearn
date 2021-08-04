d01aa045cf047efce20393c3ef3f83cae1352229
zhouhang <1024223787@qq.com>
2021-08-04 22:05:36 +0800

    vuex学习--基础项目创建

---
+++ learnvuex/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      message: "ssssss",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
